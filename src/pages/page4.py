5194f291e0fd4aa5a6a27206bf2fde6ea832dfef
KaidanS05 <kstiles2005@gmail.com>
2024-10-10 18:30:26 -0400

    remove authuser test

--- src/pages/page4.py
+++ src/pages/page4.py
@@ -6,7 +6,6 @@ display_Private_View = st.write("Hopefully only authorized users are seeing this
 
 AUTHORIZED_USERS = {
     'test@email.com',
-    'kstiles2005@gmail.com',
 }
 
 if st.experimental_user.email in AUTHORIZED_USERS:
