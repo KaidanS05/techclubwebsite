1869f679a066514e867cb178ec98a2af327aac19
KaidanS05 <kstiles2005@gmail.com>
2024-10-10 18:26:26 -0400

    page4 authorized_users

---
+++ src/pages/page4.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Page 4")
+st.write("This is the content of page 4.")
+display_Private_View = st.write("Hopefully only authorized users are seeing this.")
+
+AUTHORIZED_USERS = {
+    'test@email.com',
+    'kstiles2005@gmail.com',
+}
+
+if st.experimental_user.email in AUTHORIZED_USERS:
+    display_Private_View
